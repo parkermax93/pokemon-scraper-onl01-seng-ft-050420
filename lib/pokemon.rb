94fd07843a57c98c28a0e104b4255ac9ab51ed4e
Parker Williamson <parker.williamson@gmail.com>
2020-06-04 22:41:22 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -19,7 +19,6 @@ class Pokemon
     pokemon = database_connection.execute("SELECT * FROM pokemon WHERE id = ?", id).flatten
     name = pokemon[1]
     type = pokemon[2]
-    hp = pokemon[3]
 
     new_pokemon = Pokemon.new(id: id, name: name, type: type, db: database_connection)
    end
